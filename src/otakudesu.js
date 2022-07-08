54fb6ce29cb0f5176b741a1ae44f6d5611f9fa7b
rzkfyn <rzkfyn@gmail.com>
2022-07-08 20:35:56 +0700

    Home 50%

--- src/otakudesu.js
+++ src/otakudesu.js
@@ -1,7 +1,8 @@
 import searchAnime from './lib/searchAnime.js';
 import singleAnime from './lib/singleAnime.js';
 import getEpisodeData from './lib/episode.js';
+import getHomeData from './lib/home.js';
 
 export default {
-  searchAnime, singleAnime, getEpisodeData
+  searchAnime, singleAnime, getEpisodeData, getHomeData
 };
