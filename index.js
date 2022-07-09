0c17a55ade6c2a8ea5d6e2227682654bac1baa2b
rzkfyn <rzkfyn@gmail.com>
2022-07-09 09:20:36 +0700

    Mengizinkan origin manapun

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', routes);
 
