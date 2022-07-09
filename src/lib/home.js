b75592b01f6a4f49149dd94f362b26a2b3769187
rzkfyn <rzkfyn@gmail.com>
2022-07-09 09:19:42 +0700

    Data complete anime

--- src/lib/home.js
+++ src/lib/home.js
@@ -11,29 +11,32 @@ const getAnimeData = async () => {
   .filter(item => item.trim() !== '')
   .map(item => `<div class="venz">${item}`);
 
-  const ongoingAnime = getOngoingAnime(data[0]);
+  const ongoingAnime = scrapeOngoingAnime(data[0]);
+  const completeAnime = scrapeCompleteAnime(data[1]);
 
   return {
     ongoingAnime,
-    completedAnime: []
+    completeAnime
   };
 };
 
-const getOngoingAnime = (html) => {
+const splitAnimeEl = (html) => {
   const $ = cheerio.load(html);
+
   const animes = $('.detpost')
   .toString()
   .split('<div class="detpost">')
   .filter(item => item.trim() !== '')
   .map(item => `<div class="detpost">${item}`);
 
-  return  scrapeOngoingAnime(animes);
+  return  animes;
 };
 
 const scrapeOngoingAnime = (html) => {
+  const animes = splitAnimeEl(html)
   let res = [];
 
-  for (const anime of html) {
+  for (const anime of animes) {
     const $ = cheerio.load(anime);
 
     const judul = $('.thumb .thumbz .jdlflm').text().trim();
@@ -41,11 +44,34 @@ const scrapeOngoingAnime = (html) => {
     const poster = $('.thumb .thumbz img').attr('src');
     const url = $('.thumb a').attr('href');
     const episodeTerbaru = $('.epz').text().trim();
-    const hariTerakhirUpload = $('.epztipe').text().trim();
+    const hariUpload = $('.epztipe').text().trim();
     const tanggalTerakhirUpload = $('.newnime').text().trim();
 
     res.push({
-      judul, slug, poster, url, episodeTerbaru, hariTerakhirUpload, tanggalTerakhirUpload
+      judul, slug, poster, url, episodeTerbaru, hariUpload, tanggalTerakhirUpload
+    });
+  }
+
+  return res;
+};
+
+const scrapeCompleteAnime = (html) => {
+  const animes = splitAnimeEl(html)
+  let res = [];
+
+  for (const anime of animes) {
+    const $ = cheerio.load(anime);
+
+    const judul = $('.thumb .thumbz .jdlflm').text().trim();
+    const slug = $('.thumb a').attr('href').replace('https://otakudesu.watch/anime/', '').replace('/', '');
+    const poster = $('.thumb .thumbz img').attr('src');
+    const url = $('.thumb a').attr('href');
+    const jumlahEpisode = $('.epz').text().trim();
+    const rating = $('.epztipe').text().trim();
+    const tanggalTerakhirUpload = $('.newnime').text().trim();
+
+    res.push({
+      judul, slug, poster, url, jumlahEpisode, rating, tanggalTerakhirUpload
     });
   }
 
