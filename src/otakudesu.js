41b5df9a26075f5e82f338e0d2cdde88ab76bb08
rzkfyn <rzkfyn@gmail.com>
2022-07-17 17:33:04 +0700

    api path complete anime

--- src/otakudesu.js
+++ src/otakudesu.js
@@ -3,11 +3,13 @@ import singleAnime from './lib/singleAnime.js';
 import getEpisodeData from './lib/episode.js';
 import getHomeData from './lib/home.js';
 import getOngoingAnimeData from './lib/ongoinganime.js';
+import getCompleteAnimeData from './lib/completeanime.js';
 
 export default {
   searchAnime, 
   singleAnime, 
   getEpisodeData, 
   getHomeData,
-  getOngoingAnimeData
+  getOngoingAnimeData,
+  getCompleteAnimeData
 };
